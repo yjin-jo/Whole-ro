6115eda6f7304a17b863e26b1c571f89aba29c1a
yjin-jo <dpwls5100@icloud.com>
2023-11-24 19:23:13 +0900

    [add] : gitIgnore 반영 #3

---
+++ whole/src/main/java/model/Food.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Food {
+	private int post_id;
+
+	public int getPost_id() {
+		return post_id;
+	}
+
+	public void setPost_id(int post_id) {
+		this.post_id = post_id;
+	}
+	
+}
