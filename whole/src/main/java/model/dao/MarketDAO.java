8face0462ba798fbb2a3beb6f83086f2b6fade96
dev_me <gjalsdud1030@naver.com>
2023-11-26 00:15:51 +0900

    [add]MarketDao - findPostDetail

--- whole/src/main/java/model/dao/MarketDAO.java
+++ whole/src/main/java/model/dao/MarketDAO.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.Item;
 import model.Post;
 
 public class MarketDAO {
@@ -49,33 +50,36 @@ public class MarketDAO {
         }
         return null;
     }
-    // 글 클릭하면 상세 글 보이도록
+    // 글 제목으로 select해서 상세 글 보이도록
     // 사진, 제목, 가격, 지역, 태그, 업로드 날짜 가져오기
-    public List<Post> findPostDetail() throws SQLException {
+    public Item findPostDetail(String title) throws SQLException {
 
-    String sql = "SELECT post_id, item.cnt_like,item.detail, item.price,item.scrap, post.title, post.image,post.type "
+    String sql = "SELECT post_id, item.cnt_like,item.detail, item.price,item.scrap, post.title, post.image,post.type,post.content,post.reg_date,post.writer_id "
             + "FROM ITEM JOIN POST USING (post_id) "
-            + "WHERE post.type = 'MARKET'";
+            + "WHERE post.type = 'MARKET' and post.title = ?";
     
-    jdbcUtil.setSqlAndParameters(sql, null); // JDBCUtil에 query문 설정
-
-    
+    jdbcUtil.setSqlAndParameters(sql, new Object[]{title}); // JDBCUtil에 query문 설정
+ 
     try {
         ResultSet rs = jdbcUtil.executeQuery(); // query 실행
-       
-        List<Post> postList = new ArrayList<Post>(); // 타입이 Market인 Post 리스트 생성
-        while (rs.next()) {
-            Post post = new Post();
-            post.setPost_id(rs.getLong("post_id"));// post 객체를 생성하여 현재 행의 정보를 저장
-            post.setTitle(rs.getString("title"));
-            post.setType(rs.getString("type"));
-            post.setContent(rs.getString("content"));
-            post.setImage(rs.getString("image"));
-            post.setReg_date(rs.getDate("reg_date").toLocalDate());
-            post.setWriter_id(rs.getLong("writer_id"));
-            postList.add(post);             // List에 post 객체 저장
+   
+        if(rs.next()) {
+            Item item = new Item();
+            item.setPost_id(rs.getLong("post_id"));// post 객체를 생성하여 현재 행의 정보를 저장
+            item.setCnt_like(rs.getInt("cnt_like"));
+            item.setDetail(rs.getString("detail"));
+            item.setPrice(rs.getLong("price"));
+            item.setScrap(rs.getInt("scrap"));
+            item.setTitle(rs.getString("title"));
+            item.setImage(rs.getString("image"));
+            item.setType(rs.getString("type"));
+            item.setContent(rs.getString("content"));
+            item.setReg_date(rs.getDate("reg_date").toLocalDate());
+            item.setWriter_id(rs.getLong("writer_id"));
+
+            return item;         
         }
-        return postList;
+      
 
     } catch (Exception ex) {
         ex.printStackTrace();
--- whole/src/main/java/test/MarketTest.java
+++ whole/src/main/java/test/MarketTest.java
@@ -3,14 +3,18 @@ package test;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
+
 import model.dao.*;
+import model.Item;
 import model.Post;
 
 public class MarketTest {
     private static MarketDAO marketDao = new MarketDAO();
 
     public static void main(String[] args) throws SQLException {
-
+        Scanner scanner = new Scanner(System.in);
+        
         List<Post> postList = marketDao.findtypeMarketPost();
         // empList에 포함된 모든 emp 객체들의 필드 값을 출력
 
@@ -30,6 +34,27 @@ public class MarketTest {
         } else {
             System.out.println("No posts found.");
         }
+        
+        System.out.print("포스트제목을 입력하시오: ");
+        String posttitle = scanner.next();
+        System.out.println();
+        
+       Item item = marketDao.findPostDetail(posttitle);
+       
+       if (item != null) {   
+           System.out.println("<글 제목>");
+           System.out.println("글 제목:"+ posttitle);
+           System.out.println("글 타입:"+ item.getType());
+           System.out.println("글 내용:"+ item.getContent());
+           System.out.println("글 이미지:"+ item.getImage());
+           System.out.println("글 생성일:"+ item.getReg_date());
+           System.out.println("작성자 id:"+ item.getWriter_id());
+           System.out.println("좋아요 수:"+ item.getCnt_like());
+           System.out.println("스크랩 수:"+ item.getScrap());
+           System.out.println("가격:"+ item.getPrice());
+           System.out.println("선호 타입:"+ item.getDetail());
+           System.out.println();
+           }
     }
 
 }
