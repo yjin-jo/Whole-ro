87c22a6f77021d9ce5be6fc779596bf94f405de1
Dayun Lee <wohd7877@naver.com>
2023-11-25 16:12:46 +0900

    [feat] : userDao 수정 / User 우리 테이블에 맞게 수정

--- whole/src/main/java/model/User.java
+++ whole/src/main/java/model/User.java
@@ -8,116 +8,136 @@ package model;
  */
 
 public class User {
-	private String userId;
-	private String password;
-	private String name;
-	private String email;
-	private String phone;
-	private int commId;
-	private String commName;
+    private long userId;
+    private String email;
+    private String password;
+    private String nickname;
+    private int gender;
+    private String introdution;
+    private String interest;
+    private String address;
+    private int reportCount;
 
-	public User() { }		// 기본 생성자
-	
-	public User(String userId, String password, String name, String email, String phone, int commId) {
-		this.userId = userId;
-		this.password = password;
-		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		this.commId = commId;
-	}
-	
-	public User(String userId, String password, String name, String email, String phone, int commId, String commName) {
-		this(userId, password, name, email, phone, commId);
-		this.commName = commName;
-	}
+    public User() { }       // 기본 생성자
+    
+    public User(String nickname, String introdution, String interest) { 
+        this.nickname = nickname;
+        this.introdution = introdution;
+        this.interest = interest; 
+    }  
+    
+    public User(long userId, String email, String password, String nickname, int gender, String introdution,
+            String interest, String address, int reportCount) {
+        super();
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.introdution = introdution;
+        this.interest = interest;
+        this.address = address;
+        this.reportCount = reportCount;
+    }
 
-	public User(String userId, String name, String email, String phone) {
-		this.userId = userId;
-		this.name = name;
-		this.email = email;
-		this.phone = phone;		
-	}
-	
-	/*public void update(User updateUser) {
+
+    /*public void update(User updateUser) {
         this.password = updateUser.password;
         this.name = updateUser.name;
         this.email = updateUser.email;
         this.phone = updateUser.phone;
     }*/
-	
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public int getCommId() {
-		return commId;
-	}
-
-	public void setCommId(int commId) {
-		this.commId = commId;
-	}
-
-	public String getCommName() {
-		return commName;
-	}
-
-	public void setCommName(String commName) {
-		this.commName = commName;
-	}
-
-	
-	/* 비밀번호 검사 */
-	public boolean matchPassword(String password) {
-		if (password == null) {
-			return false;
-		}
-		return this.password.equals(password);
-	}
-	
-	public boolean isSameUser(String userid) {
-        return this.userId.equals(userid);
+    public long getUserId() {
+        return userId;
     }
 
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + ", phone="
-				+ phone + ", commId=" + commId + "]";
-	}	
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getIntrodution() {
+        return introdution;
+    }
+
+    public void setIntrodution(String introdution) {
+        this.introdution = introdution;
+    }
+
+    public String getInterest() {
+        return interest;
+    }
+
+    public void setInterest(String interest) {
+        this.interest = interest;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getReportCount() {
+        return reportCount;
+    }
+
+    public void setReportCount(int reportCount) {
+        this.reportCount = reportCount;
+    }
+
+    
+    /* 비밀번호 검사 */
+    public boolean matchPassword(String password) {
+        if (password == null) {
+            return false;
+        }
+        return this.password.equals(password);
+    }
+    
+
+    public boolean isSameUser(int userid) {
+        return this.userId == userid;
+    }
+
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", email=" + email + ", password=" + password + ", nickname=" + nickname + ", gender=" + gender + 
+                ", introdution=" + introdution + ", interest=" + interest + ", address=" + address 
+                + ", reportCount=" + reportCount + "]";
+    }   
 }
+
