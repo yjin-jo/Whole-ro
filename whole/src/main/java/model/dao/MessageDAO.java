3a85f561f8c91713bc3dc2329e7975de9290687b
Parkseojin <seojin5565@gmail.com>
2023-11-25 19:29:10 +0900

    [feat] : messageDAO 메시지 개별 리턴, 메시지 전체 리턴까지 #17

--- whole/src/main/java/model/dao/MessageDAO.java
+++ whole/src/main/java/model/dao/MessageDAO.java
@@ -2,11 +2,16 @@ package model.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import model.Food;
-import model.Message;
+import model.MessageEntity;
+import model.enums.BooleanEnum;
+import model.enums.MessageType;
 
 public class MessageDAO {
 private JDBCUtil jdbcUtil = null;
@@ -14,26 +19,73 @@ private JDBCUtil jdbcUtil = null;
     public MessageDAO() {          
         jdbcUtil = new JDBCUtil();  // JDBCUtil 객체 생성
     }
+    
+    public String printMessageSenderNickName(Long id) {
+        String sql = "SELECT nickname "
+                + "FROM user_table "
+                + "where user_id=? ";
+        
+     jdbcUtil.setSqlAndParameters(sql, new Object[] { id });
 
-    public List<Message> messageList() throws SQLException {
-        String sql = "SELECT post_id, title, image, exp_date, reg_date "
-                + "FROM FOOD JOIN POST USING (post_id) "
-                + "ORDER BY reg_date";        
-     jdbcUtil.setSqlAndParameters(sql, null);        // JDBCUtil에 query문 설정
-                 
      try {
          ResultSet rs = jdbcUtil.executeQuery();         // query 실행         
-         List<Food> foodList = new ArrayList<Food>();    // Community들의 리스트 생성
-         while (rs.next()) {
-             Food food = new Food();
-             food.setPost_id(rs.getLong("post_id"));// Community 객체를 생성하여 현재 행의 정보를 저장
-             food.setTitle(rs.getString("title"));
-             food.setImage(rs.getString("image"));
-             food.setExp_date(rs.getDate("exp_date"));
-             food.setReg_date(rs.getDate("reg_date"));
-             foodList.add(food);             // List에 Community 객체 저장
+         String nickname = "";
+         if (rs.next()) {
+            nickname = rs.getString("nickname");
          }       
-         return foodList;                    
+         
+         return nickname;                    
+         
+     } catch (Exception ex) {
+         ex.printStackTrace();
+     } finally {
+         jdbcUtil.close();       // resource 반환
+     }
+     return null;
+        
+    }
+
+    public List<MessageEntity> printMessageList(Long id) throws SQLException {
+        String sql = "SELECT msg_id, sender_id, title, reg_date "
+                + "FROM message "
+                + "where receiver_id = ? ";
+        
+     jdbcUtil.setSqlAndParameters(sql, new Object[] { id });        // JDBCUtil에 query문 설정
+                 
+//     
+//     private Long msgId;
+//     private Long senderId;
+//     private Long receiverId;
+//     private String title;
+//     private String content;
+//     private String image;
+//     private Date reg_date;
+//     private BooleanEnum is_read;
+//     private MessageType msgType;
+     
+     
+     try {
+         ResultSet rs = jdbcUtil.executeQuery();         // query 실행         
+         List<MessageEntity> messageList = new ArrayList<MessageEntity>();    // Community들의 리스트 생성
+         
+         while (rs.next()) {
+     
+             
+             MessageEntity message = new MessageEntity(
+                     rs.getLong("msg_id"),
+                     rs.getLong("sender_id"),
+                     null,
+                     rs.getString("title"),
+                     null,
+                     null, 
+                     rs.getDate("reg_date").toLocalDate(),
+                     null,
+                     null
+                     );
+             messageList.add(message);
+         }       
+         
+         return messageList;                    
          
      } catch (Exception ex) {
          ex.printStackTrace();
@@ -42,4 +94,82 @@ private JDBCUtil jdbcUtil = null;
      }
      return null;
  }
+   
+    public MessageEntity printMessage(Long id) throws SQLException {
+        String sql = "SELECT sender_id, title, content, image, reg_date "
+                + "FROM message "
+                + "where msg_id = ? ";
+        
+     jdbcUtil.setSqlAndParameters(sql, new Object[] { id });        // JDBCUtil에 query문 설정
+               
+     try {
+         ResultSet rs = jdbcUtil.executeQuery();         // query 실행                  
+         if (rs.next()) {
+             MessageEntity message = new MessageEntity(
+                     id,
+                     rs.getLong("sender_id"),
+                     null,
+                     rs.getString("title"),
+                     rs.getString("content"),
+                     rs.getString("image"),
+                     rs.getDate("reg_date").toLocalDate(),
+                     null,
+                     null
+                     );
+             return message;
+         }                        
+         
+     } catch (Exception ex) {
+         ex.printStackTrace();
+     } finally {
+         jdbcUtil.close();       // resource 반환
+     }
+     return null;
+ }
+    
+    
+    TITLE, CONTENT, IMAGE, REG_DATE, IS_READ, MSG_TYPE, MSG_ID, SENDER_ID, RECEIVER_ID, POST_ID
+    
+   public MessageEntity sendMessage(MessageEntity message) throws SQLException {
+       
+        String title = message.getTitle();
+        String content = message.getContent();
+        LocalDate regDate = message.getRegDate();
+        BooleanEnum is_read = message.getIsRead();
+        
+        
+        
+        
+        String sql = "INSERT INTO message "
+                + "(TITLE, CONTENT, IMAGE, REG_DATE, IS_READ, MSG_TYPE, MSG_ID, SENDER_ID, RECEIVER_ID, POST_ID) "
+                + "VAULES(? ,?, ?, ?, ?, ?, SEQUENCE_MESSAGEID.nextval, ?, ?, ?)  ";
+                
+        
+     jdbcUtil.setSqlAndParameters(sql, new Object[] { id });        // JDBCUtil에 query문 설정
+               
+     try {
+         ResultSet rs = jdbcUtil.executeQuery();         // query 실행                  
+         if (rs.next()) {
+             MessageEntity message = new MessageEntity(
+                     id,
+                     rs.getLong("sender_id"),
+                     null,
+                     rs.getString("title"),
+                     rs.getString("content"),
+                     rs.getString("image"),
+                     rs.getDate("reg_date").toLocalDate(),
+                     null,
+                     null
+                     );
+             return message;
+         }                        
+         
+     } catch (Exception ex) {
+         ex.printStackTrace();
+     } finally {
+         jdbcUtil.close();       // resource 반환
+     }
+     return null;
+ }
+   
 }
