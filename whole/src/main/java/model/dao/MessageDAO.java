bf1f52851e04c09f16ca70c29205e8ed219b3202
Parkseojin <seojin5565@gmail.com>
2023-11-25 16:39:35 +0900

    [feat] : Messag messageList 메소드 추가 #17

--- whole/src/main/java/model/dao/MessageDAO.java
+++ whole/src/main/java/model/dao/MessageDAO.java
@@ -1,5 +1,45 @@
 package model.dao;
 
-public class MessageDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Food;
+import model.Message;
+
+public class MessageDAO {
+private JDBCUtil jdbcUtil = null;
+    
+    public MessageDAO() {          
+        jdbcUtil = new JDBCUtil();  // JDBCUtil 객체 생성
+    }
+
+    public List<Message> messageList() throws SQLException {
+        String sql = "SELECT post_id, title, image, exp_date, reg_date "
+                + "FROM FOOD JOIN POST USING (post_id) "
+                + "ORDER BY reg_date";        
+     jdbcUtil.setSqlAndParameters(sql, null);        // JDBCUtil에 query문 설정
+                 
+     try {
+         ResultSet rs = jdbcUtil.executeQuery();         // query 실행         
+         List<Food> foodList = new ArrayList<Food>();    // Community들의 리스트 생성
+         while (rs.next()) {
+             Food food = new Food();
+             food.setPost_id(rs.getLong("post_id"));// Community 객체를 생성하여 현재 행의 정보를 저장
+             food.setTitle(rs.getString("title"));
+             food.setImage(rs.getString("image"));
+             food.setExp_date(rs.getDate("exp_date"));
+             food.setReg_date(rs.getDate("reg_date"));
+             foodList.add(food);             // List에 Community 객체 저장
+         }       
+         return foodList;                    
+         
+     } catch (Exception ex) {
+         ex.printStackTrace();
+     } finally {
+         jdbcUtil.close();       // resource 반환
+     }
+     return null;
+ }
 }
